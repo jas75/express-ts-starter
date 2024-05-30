1284341d25f08e0b382a16a6b306bcda2a54ba90
Jas <azoulayjason@gmail.com>
2024-05-30 18:59:28 +0200

    refactor authservice

--- src/ressources/auth/auth.controller.ts
+++ src/ressources/auth/auth.controller.ts
@@ -1,21 +1,15 @@
 import { Request, Response, Router } from 'express';
-// import { AuthService } from './auth.service';
-import pool from '../../database';
-import bcrypt from 'bcrypt';
+import { AuthService } from './auth.service';
 import passport from '../../services/passport/passport-config';
-import jwt from 'jsonwebtoken';
 import { config } from '../../config';
 const AuthController = Router();
-// const service: AuthService = new AuthService();
+const service: AuthService = new AuthService();
 
 AuthController.post('/register', async (req: Request, res: Response) => {
   const { username, password } = req.body;
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [
-      username,
-      hashedPassword
-    ]);
+    const hashedPassword = await service.hashPassword(password);
+    const result = await service.registertUser(username, hashedPassword);
     res.status(201).json(result.rows[0]);
   } catch (err) {
     res.status(500).json({ error: 'Database error', details: err });
@@ -30,9 +24,7 @@ AuthController.post('/login', (req: Request, res: Response, next) => {
     if (!user) {
       return res.status(401).json({ error: 'Invalid credentials', details: info });
     }
-    const token = jwt.sign({ userId: user.id, username: user.username }, config.JWT_SECRET as string, {
-      expiresIn: '1h'
-    });
+    const token = service.createJWT(user.id, user.username, config.JWT_SECRET as string);
     return res.json({ token });
   })(req, res, next);
 });
--- src/ressources/auth/auth.service.ts
+++ src/ressources/auth/auth.service.ts
@@ -1,1 +1,34 @@
-export class AuthService {}
+import bcrypt from 'bcrypt';
+import { QueryResult } from 'pg';
+import pool from '../../database';
+import jwt from 'jsonwebtoken';
+
+export class AuthService {
+  public async registertUser(username: string, hashedPassword: string): Promise<QueryResult<any>> {
+    try {
+      const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [
+        username,
+        hashedPassword
+      ]);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public createJWT(userId: number, username: string, secret: string) {
+    const token = jwt.sign({ userId: userId, username: username }, secret, {
+      expiresIn: '1h'
+    });
+    return token;
+  }
+
+  public async hashPassword(password: string): Promise<string> {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return hashedPassword;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
