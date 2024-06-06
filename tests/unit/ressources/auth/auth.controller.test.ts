5990556a0e95b88c207899087cf6efeb17ceaa45
Jas <azoulayjason@gmail.com>
2024-06-07 01:17:37 +0200

    test protected routes and login

--- tests/unit/ressources/auth/auth.controller.test.ts
+++ tests/unit/ressources/auth/auth.controller.test.ts
@@ -5,7 +5,10 @@ import { pool } from '../../../../src/database';
 
 
 describe('AuthController', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let token: any;
   afterAll(async () => {
+    await pool.query(`DELETE FROM users WHERE username = 'testUser'`);
     await pool.end();
     await server.close();
   });
@@ -20,48 +23,44 @@ describe('AuthController', () => {
     it('should return 500 if service throws an error', async () => {
       const err = await request(app).post('/auth/register').send({ username: 'testUser', password: 'password' });
       expect(err.status).toBe(500);
-      await pool.query(`DELETE FROM users WHERE username = 'testUser'`);
     });
   });
 
-  // describe('POST /auth/login', () => {
-  //   it('should log in a user', async () => {
-  //     const authService = new AuthService();
-  //     const hashedPassword = await authService.hashPassword('password');
-  //     await authService.registertUser('testuser', hashedPassword);
+  describe('POST /auth/login', () => {
+    it('should log in a user', async () => {
+      const response = await request(app).post('/auth/login').send({ username: 'testUser', password: 'password' });
+      token = response.body.token;
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('token');
+    });
 
-  //     const response = await request(app).post('/auth/login').send({ username: 'testuser', password: 'password' });
+    it('should return 401 if credentials are invalid', async () => {
+      const response = await request(app)
+        .post('/auth/login')
+        .send({ username: 'wronguser', password: 'wrongpassword' });
 
-  //     expect(response.status).toBe(200);
-  //     expect(response.body).toHaveProperty('token');
-  //   });
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('error');
+    });
+  });
 
-  //   it('should return 401 if credentials are invalid', async () => {
-  //     const response = await request(app)
-  //       .post('/auth/login')
-  //       .send({ username: 'wronguser', password: 'wrongpassword' });
+  describe('GET /auth/protected', () => {
+    it('should access a protected route', async () => {
+      // const authService = new AuthService();
+      // const hashedPassword = await authService.hashPassword('password');
+      // const result = await authService.registertUser('testuser', hashedPassword);
+      // const token = authService.createJWT(result.rows[0].id, 'testuser', 'your_jwt_secret');
+      // const token = service.createJWT(user.id, user.username, config.JWT_SECRET as string);
 
-  //     expect(response.status).toBe(401);
-  //     expect(response.body).toHaveProperty('error');
-  //   });
-  // });
+      const response = await request(app).get('/auth/protected').set('Authorization', `Bearer ${token}`);
 
-  // describe('GET /auth/protected', () => {
-  //   it('should access a protected route', async () => {
-  //     const authService = new AuthService();
-  //     const hashedPassword = await authService.hashPassword('password');
-  //     const result = await authService.registertUser('testuser', hashedPassword);
-  //     const token = authService.createJWT(result.rows[0].id, 'testuser', 'your_jwt_secret');
+      expect(response.status).toBe(200);
+      // expect(response.text).toBe('This is a protected route');
+    });
 
-  //     const response = await request(app).get('/auth/protected').set('Authorization', `Bearer ${token}`);
-
-  //     expect(response.status).toBe(200);
-  //     expect(response.text).toBe('This is a protected route');
-  //   });
-
-  //   it('should return 401 for unauthenticated requests', async () => {
-  //     const response = await request(app).get('/auth/protected');
-  //     expect(response.status).toBe(401);
-  //   });
-  // });
+    it('should return 401 for unauthenticated requests', async () => {
+      const response = await request(app).get('/auth/protected');
+      expect(response.status).toBe(401);
+    });
+  });
 });
